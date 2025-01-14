a571e8ca861539b6398ada806e3dd1747988e96e
Alexander <name.way@gmail.com>
2025-01-14 18:42:43 +0300

    add token to LS and redirect

--- src/pages/auth/AuthForm/LoginForm.tsx/LoginForm.tsx
+++ src/pages/auth/AuthForm/LoginForm.tsx/LoginForm.tsx
@@ -4,6 +4,8 @@ import cn from 'classnames'
 import styles from './LoginForm.module.scss'
 import React, { useEffect, useState } from 'react'
 import { backendBaseUrl } from '@/shared/config/backend'
+import { LOCALSTORAGE_USER_TOKEN } from "@/shared/consts/consts"
+import { useRouter } from "next/navigation"
 
 type Inputs = {
     [email: string]: string
@@ -18,8 +20,8 @@ type LoginFormProps = {
 export const LoginForm = (props: LoginFormProps) => {
 
     const { className } = props
-
     const [isSignUp, setIsSugnUp] = useState(false)
+    const router = useRouter()
 
     const {
         register,
@@ -36,103 +38,130 @@ export const LoginForm = (props: LoginFormProps) => {
         const formData = new FormData()
 
         for (const key in data) {
-                formData.append(key, data[key])
-            }
-            try {
-                const response = await fetch(`${backendBaseUrl}/api/user/registration`,
-                    {
-                        method: 'POST',
-                        body: formData
-                    })
-
-                console.log(await response.json());
-
-            } catch (error) {
-                console.log(error);
-
-            }
+            formData.append(key, data[key])
         }
+        try {
+            const response = await fetch(`${backendBaseUrl}/api/user/registration`,
+                {
+                    method: 'POST',
+                    body: formData
+                })
 
-        const switchLoginOrRegisterHandler = () => {
-            setIsSugnUp(prev => !prev)
+            const user = await response.json();
+            localStorage.setItem(LOCALSTORAGE_USER_TOKEN, user.token)
+            // TODO добавить сообщение об успехе
+            router.push('/')
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    const onSignInSubmit: SubmitHandler<Inputs> = async (data) => {
+        console.log(data, typeof data)
+        const formData = new FormData()
+
+        for (const key in data) {
+            formData.append(key, data[key])
+        }
+        try {
+            const response = await fetch(`${backendBaseUrl}/api/user/login`,
+                {
+                    method: 'POST',
+                    body: formData
+                })
+
+            const user = await response.json();
+            localStorage.setItem(LOCALSTORAGE_USER_TOKEN, user.token)
+            router.push('/')
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    const switchLoginOrRegisterHandler = () => {
+        setIsSugnUp(prev => !prev)
+        reset({
+            username: '',
+            email: '',
+            password: ''
+        })
+    }
+
+    useEffect(() => {
+        if (isSubmitSuccessful) {
             reset({
                 username: '',
                 email: '',
                 password: ''
             })
         }
+    },
+        [])
 
-        useEffect(() => {
-            if (isSubmitSuccessful) {
-                reset({
-                    username: '',
-                    email: '',
-                    password: ''
-                })
+    return (
+        <form
+            onSubmit={isSignUp ? handleSubmit(onSignUpSubmit) : handleSubmit(onSignInSubmit)}
+            className={cn(styles.loginForm, className)}>
+            {isSignUp &&
+                <>
+                    <label htmlFor="username">Имя пользователя</label>
+                    <input
+                        placeholder='user'
+                        id='username'
+                        className={styles.loginInput}
+                        {...register("username",
+                            {
+                                required: 'Поле обязательно',
+                                minLength: {
+                                    value: 4,
+                                    message: 'Минимум 4 символа'
+                                }
+                            })}
+                    />
+                    {errors.username && <span className={styles.errorText}>{errors.username.message}</span>}
+                </>
             }
-        },
-            [])
 
-        return (
-            <form
-                onSubmit={handleSubmit(onSignUpSubmit)}
-                className={cn(styles.loginForm, className)}>
-                {isSignUp &&
-                    <>
-                        <label htmlFor="username">Имя пользователя</label>
-                        <input
-                            placeholder='user'
-                            id='username'
-                            className={styles.loginInput}
-                            {...register("username",
-                                {
-                                    required: 'Поле обязательно',
-                                    minLength: {
-                                        value: 4,
-                                        message: 'Минимум 4 символа'
-                                    }
-                                })}
-                        />
-                        {errors.username && <span className={styles.errorText}>{errors.username.message}</span>}
-                    </>
-                }
+            <label htmlFor="email">Почта</label>
+            <input
+                placeholder='user@example.ru'
+                id='email'
+                className={styles.loginInput}
+                type='email'
+                {...register("email", {
+                    required: 'Поле обязательно',
+                    pattern: {
+                        value: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.]{1}[a-zA-Z]{2,}$/,
+                        message: 'Неверно указана почта'
+                    }
+                })}
 
-                <label htmlFor="email">Почта</label>
-                <input
-                    placeholder='user@example.ru'
-                    id='email'
-                    className={styles.loginInput}
-                    type='email'
-                    {...register("email", {
-                        required: 'Поле обязательно',
-                        pattern: {
-                            value: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.]{1}[a-zA-Z]{2,}$/,
-                            message: 'Неверно указана почта'
-                        }
-                    })}
+            />
+            {errors.email && <span className={styles.errorText}>{errors.email.message}</span>}
 
-                />
-                {errors.email && <span className={styles.errorText}>{errors.email.message}</span>}
+            <label htmlFor="password">Пароль</label>
+            <input
+                placeholder='password'
+                id='password'
+                className={styles.loginInput}
+                type='password'
+                {...register("password", { required: 'Поле обязательно' })} />
+            {errors.password && <span className={styles.errorText}>{errors.password.message}</span>}
 
-                <label htmlFor="password">Пароль</label>
-                <input
-                    placeholder='password'
-                    id='password'
-                    className={styles.loginInput}
-                    type='password'
-                    {...register("password", { required: 'Поле обязательно' })} />
-                {errors.password && <span className={styles.errorText}>{errors.password.message}</span>}
+            <div className={styles.buttons}>
+                <Button className={styles.register} variant={ButtonVariant.CLEAR} shape={ButtonShape.TEXT} onClick={switchLoginOrRegisterHandler}>
+                    {isSignUp
+                        ? 'Войти в аккаунт'
+                        : 'Регистрация'
+                    }
+                </Button>
+                <input type="submit" className={styles.submitInput} value={isSignUp ? 'Регистрация' : 'Войти'} />
+            </div>
 
-                <div className={styles.buttons}>
-                    <Button className={styles.register} variant={ButtonVariant.CLEAR} shape={ButtonShape.TEXT} onClick={switchLoginOrRegisterHandler}>
-                        {isSignUp
-                            ? 'Войти в аккаунт'
-                            : 'Регистрация'
-                        }
-                    </Button>
-                    <input type="submit" className={styles.submitInput} value={isSignUp ? 'Регистрация' : 'Войти'} />
-                </div>
-
-            </form>
-        )
-    }
+        </form>
+    )
+}
--- src/shared/consts/consts.ts
+++ src/shared/consts/consts.ts
@@ -1,1 +1,2 @@
 export const LOCALSTORAGE_USER_KEY = 'user';
+export const LOCALSTORAGE_USER_TOKEN = 'token';
